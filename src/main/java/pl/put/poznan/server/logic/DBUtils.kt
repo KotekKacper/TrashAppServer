77d047e203b7567872c697373caa3ad18a8456cd
Kacper Garncarek <kacpergarncarek@gmail.com>
2023-01-22 00:30:22 +0100

    Connection to the exact database established

--- src/main/java/pl/put/poznan/server/logic/DBUtils.kt
+++ src/main/java/pl/put/poznan/server/logic/DBUtils.kt
@@ -2,10 +2,80 @@ package pl.put.poznan.server.logic
 
 import org.slf4j.LoggerFactory
 import pl.put.poznan.server.rest.Controller
+import java.sql.*
+import java.util.*
 
 class DBUtils {
 
     private val logger = LoggerFactory.getLogger(DBUtils::class.java)
+    private var dbUsername = "user" // provide the username
+    private var dbPassword = "userpass" // provide the corresponding password
+    private var conn: Connection? = getConnection(dbUsername, dbPassword)
+
+    private fun getConnection(username: String, password: String): Connection? {
+        val connectionProps = Properties()
+        connectionProps.put("user", username)
+        connectionProps.put("password", password)
+        try {
+            return DriverManager.getConnection(
+                "jdbc:" + "mysql" + "://" +
+                        "127.0.0.1" +
+                        ":" + "3306" + "/TrashApp" +
+                        "",
+                connectionProps
+            )
+        } catch (ex: SQLException) {
+            // handle any SQL errors
+            ex.printStackTrace()
+        } catch (ex: Exception) {
+            // handle any errors
+            ex.printStackTrace()
+        }
+        return null
+    }
+
+    private fun executeMySQLQuery() {
+        var stmt: Statement? = null
+        var resultset: ResultSet? = null
+
+        try {
+            stmt = conn!!.createStatement()
+            resultset = stmt!!.executeQuery("select * from trash;")
+
+            while (resultset!!.next()) {
+                logger.debug(resultset.getString("user_login_report"))
+            }
+        } catch (ex: SQLException) {
+            // handle any errors
+            ex.printStackTrace()
+        } finally {
+            // release resources
+            if (resultset != null) {
+                try {
+                    resultset.close()
+                } catch (sqlEx: SQLException) {
+                }
+
+                resultset = null
+            }
+
+            if (stmt != null) {
+                try {
+                    stmt.close()
+                } catch (sqlEx: SQLException) {
+                }
+
+                stmt = null
+            }
+
+            if (conn != null) {
+                try {
+                    conn!!.close()
+                } catch (sqlEx: SQLException) {
+                }
+            }
+        }
+    }
 
     fun functionSelector(sqlFun: String, data: String): String{
         // log the parameters
@@ -19,7 +89,7 @@ class DBUtils {
 
     private fun getCompanies(data: String): String{
 
-
+        executeMySQLQuery()
         return "company@comp.com;;0;;;;"
     }
 }
